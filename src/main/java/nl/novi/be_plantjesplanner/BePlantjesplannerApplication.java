d9d1d3407909c7f25a0321d4bb6532bebaf56b8b
szuiderweg <svmzuiderweg@hotmail.com>
2024-04-01 10:34:03 +0200

    first local commit

---
+++ src/main/java/nl/novi/be_plantjesplanner/BePlantjesplannerApplication.java
@@ -0,0 +1,14 @@
+package nl.novi.be_plantjesplanner;
+//dit is een test wijziging
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BePlantjesplannerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BePlantjesplannerApplication.class, args);
+    }
+
+}
---
+++ src/test/java/nl/novi/be_plantjesplanner/BePlantjesplannerApplicationTests.java
@@ -0,0 +1,13 @@
+package nl.novi.be_plantjesplanner;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BePlantjesplannerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
